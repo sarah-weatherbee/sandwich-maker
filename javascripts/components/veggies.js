9037ee3481b5351ccb6236b03011744632728100
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-04-11 20:49:22 -0500

    created ingredient modules

---
+++ javascripts/components/veggies.js
@@ -0,0 +1,12 @@
+const veggiesInfo = {
+    peppers: 1,
+    pickles: 1,
+    tomato: 2,
+    olives: 3
+};
+
+const getVeggies = () => {
+    return veggiesInfo;
+};
+
+export default { getVeggies };
